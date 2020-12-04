a25227db5a6b02aeaaff6a6920cc90834594fceb
Eddie Chan <eddiecwh@gmail.com>
2020-12-04 13:21:59 -0600

    Update server.js
    Activate syncing for MySQL database, w/o dropping pre-existing data

--- src/server.js
+++ src/server.js
@@ -10,10 +10,12 @@ const cors = require("cors");
 const app = express();
 
 const db = require("./app/models");
-// db.sequelize.sync();
-db.sequelize.sync({ force: true }).then(() => {
-    console.log("Drop and re-sync db.");
-  });
+
+db.sequelize.sync();
+
+// db.sequelize.sync({ force: true }).then(() => {
+//     console.log("Drop and re-sync db.");
+//   });
 
 var corsSettings = {
     origin: "http://localhost:8081"
